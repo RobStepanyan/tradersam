e6c1e665be31e1c36170cb723fb1608e4c106b25
RobStepanyan <robert1stepanyan@gmail.com>
2020-02-15 21:04:10 +0400

    Asset details, news details routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('asset/', views.asset_details, name='asset-details'),
+    path('news/', views.news_details, name='asset-details'),
     path('error400/', views.error400, name='error400'),
     path('error403/', views.error403, name='error403'),
     path('error404/', views.error404, name='error404'),
