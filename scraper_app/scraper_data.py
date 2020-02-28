f5cd2bc05eaba2d047a333f381e2250e2ed4e2c6
RobStepanyan <robert1stepanyan@gmail.com>
2020-02-28 22:33:10 +0400

    Minor changes

--- scraper_app/scraper_data.py
+++ scraper_app/scraper_data.py
@@ -89,12 +89,12 @@ before_live_fields = ['Prev. Close']
 # 'High', 'Chg (7D)', 'Time']
 live_fields = {
     'Commodities': ['Month', 'Last', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'],
-    'Currencies': ['Last', 'Open', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'], # Dont store Open
-    'Cryptocurrencies': ['Price (USD)', 'Market Cap', 'Vol (24H)', 'Total Vol', 'Chg (24H)', 'Chg (7D)'], # Dont store Open
+    'Currencies': ['Last', 'Open', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'], 
+    'Cryptocurrencies': ['Last', 'Market Cap', 'Vol.', 'Total Vol.', 'Chg. %', 'Chg (7D)'], 
     'Stocks': ['Last', 'High', 'Low', 'Chg.', 'Chg. %', 'Vol.', 'Time'],
     'Indices': ['Last', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'],
     'ETFs': ['Last', 'Chg. %', 'Vol.', 'Time'],
-    'Bonds': ['Yield', 'Prev.', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'],
+    'Bonds': ['Yield', 'Prev. Close', 'High', 'Low', 'Chg.', 'Chg. %', 'Time'],
     'Funds': ['Last', 'Chg. %', 'Total Assets', 'Time']
 }
 
