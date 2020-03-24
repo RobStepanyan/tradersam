d398ae618e2a82bead89608e4844f8034bfab1cf
RobStepanyan <robert1stepanyan@gmail.com>
2020-03-25 00:14:58 +0400

    All Asssets table no data found text

--- main_app/static/main_app/js/table.js
+++ main_app/static/main_app/js/table.js
@@ -36,35 +36,39 @@ $(function(){
             success: function(data){
                 container.empty()
                 
-                container.append('<thead><tr></tr></thead>')
-                data['fields'].forEach(e => {
-                    $('.market-table tr').append('<th>'+e+'</th>')
-                });
-                container.append('<tbody></tbody>')
-                data['data_list'].forEach(e => {
-                    s = ''
-                    var hrf = '/dev/asset/' + e['static']['country'].toLowerCase() + '/' + e['static']['Type'] + '/' + e['static']['id'] + '/'
-                    s += '<td><a href="' + hrf + '">'+ Object.values(e['live'])[0] + '</a></td>'
-                    Object.values(e['live']).slice(1).forEach(v => {
-                        if (v.includes('+')) {
-                            if (v.includes('%')) {
-                                s += '<td><span class="ml-0 d-initial change up">'+ v + '</span></td>'
-                            } else {
-                                s += '<td><span class="ml-0 d-initial text-success">'+ v + '</span></td>'
-                            }
-                        } else if (v.includes('-')) {
-                            if (v.includes('%')) {
-                                s += '<td><span class="ml-0 d-initial change down">'+ v + '</span></td>'
-                            } else {
-                                s += '<td><span class="ml-0 d-initial text-danger">'+ v + '</span></td>'
-                            }
-                        } else {
-                            s += '<td>'+ v + '</td>'
-                        };
+                if (data['data_list'].length != 0){
+                    container.append('<thead><tr></tr></thead>')
+                    data['fields'].forEach(e => {
+                        $('.market-table tr').append('<th>'+e+'</th>')
                     });
-                    $('.market-table tbody').append('<tr>' + s + '</tr>')
-                
-                });
+                    container.append('<tbody></tbody>')
+                    data['data_list'].forEach(e => {
+                        s = ''
+                        var hrf = '/dev/asset/' + e['static']['country'].toLowerCase() + '/' + e['static']['Type'] + '/' + e['static']['id'] + '/'
+                        s += '<td><a href="' + hrf + '">'+ Object.values(e['live'])[0] + '</a></td>'
+                        Object.values(e['live']).slice(1).forEach(v => {
+                            if (v.includes('+')) {
+                                if (v.includes('%')) {
+                                    s += '<td><span class="ml-0 d-initial change up">'+ v + '</span></td>'
+                                } else {
+                                    s += '<td><span class="ml-0 d-initial text-success">'+ v + '</span></td>'
+                                }
+                            } else if (v.includes('-')) {
+                                if (v.includes('%')) {
+                                    s += '<td><span class="ml-0 d-initial change down">'+ v + '</span></td>'
+                                } else {
+                                    s += '<td><span class="ml-0 d-initial text-danger">'+ v + '</span></td>'
+                                }
+                            } else {
+                                s += '<td>'+ v + '</td>'
+                            };
+                        });
+                        $('.market-table tbody').append('<tr>' + s + '</tr>')
+                    
+                    });
+                } else {
+                    $('<h5 class="text-white text-center py-3 mb-0">No data found</h5>').appendTo(container);
+                };
             }
         });
     };
