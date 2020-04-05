2b20829ed4db65772824f4cbb20fb7154441aa84
RobStepanyan <robert1stepanyan@gmail.com>
2020-04-05 15:23:55 +0400

    Fixed /account account-info, watchlists responsive

--- static/main_app/js/account.js
+++ static/main_app/js/account.js
@@ -4,19 +4,21 @@ $(function () {
 // ........
 
 var container = $('#main-container')
+var getLoadDebounce = _.debounce(function(){loadHTML();getData();}, 250);
 loadHTML()
 getData()
 
-$('.tab').click(_.debounce(function () {
-    container.empty()
-    $('<div class="h-100 lds-dual-ring-md"></div>').appendTo(container)
-    loadHTML()
-    getData()
-}, 250));
+$('.tab').click(function(){
+  container.empty()
+  container.addClass('d-flex')
+  $('<div class="h-100 lds-dual-ring-md m-auto"></div>').appendTo(container)
+  getLoadDebounce()
+})
 
 
 function loadHTML() {
     container.empty()
+    container.removeClass('d-flex')
     container.addClass('d-none')
     dep = $('.tab.active').attr('id')
     if (dep == 'account-info') {
@@ -98,47 +100,79 @@ var html_templates = {
     `
     <div class="row">
       <div class="col-lg-10 mx-auto">
-
         <div class="row" id="watchlist-row">
           <div class="col-lg-6">
             <div class="account-card watchlist">
               <h3>Watchlist Name</h3>
               <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Symbol</th>
+                    <th>Change %</th>
+                    <th>Volume</th>
+                  </tr>
+                </thead>
                 <tbody>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">AKS</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.76%</span></td>
+                    <td>12.79M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CIEN</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.33%</span></td>
+                    <td>2.63M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CLF</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.40%</span></td>
+                    <td>17.39M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">FCX</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.63%</span></td>
+                    <td>68.00M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">INTC</a></td>
+                    <td><span class="ml-0 d-initial change down">-0.56%</span></td>
+                    <td>60.73M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">RIG</a></td>
+                    <td><span class="ml-0 d-initial change up">+11.30%</span></td>
+                    <td>38.90M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">S</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.18%</span></td>
+                    <td>43.60M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">T</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.43%</span></td>
+                    <td>121.10M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">TEVA</a></td>
+                    <td><span class="ml-0 d-initial change down">-5.57%</span></td>
+                    <td>30.15M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">X</a></td>
+                    <td><span class="ml-0 d-initial change up">+3.22%</span></td>
+                    <td>21.49M</td>
                   </tr>
                 </tbody>
               </table>
@@ -148,88 +182,151 @@ var html_templates = {
             <div class="account-card watchlist">
               <h3>Watchlist Name</h3>
               <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Symbol</th>
+                    <th>Change %</th>
+                    <th>Volume</th>
+                  </tr>
+                </thead>
                 <tbody>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">AKS</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.76%</span></td>
+                    <td>12.79M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CIEN</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.33%</span></td>
+                    <td>2.63M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CLF</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.40%</span></td>
+                    <td>17.39M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">FCX</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.63%</span></td>
+                    <td>68.00M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">INTC</a></td>
+                    <td><span class="ml-0 d-initial change down">-0.56%</span></td>
+                    <td>60.73M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">RIG</a></td>
+                    <td><span class="ml-0 d-initial change up">+11.30%</span></td>
+                    <td>38.90M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">S</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.18%</span></td>
+                    <td>43.60M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">T</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.43%</span></td>
+                    <td>121.10M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">TEVA</a></td>
+                    <td><span class="ml-0 d-initial change down">-5.57%</span></td>
+                    <td>30.15M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">X</a></td>
+                    <td><span class="ml-0 d-initial change up">+3.22%</span></td>
+                    <td>21.49M</td>
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
-        </div>
-    
-        <div class="row" id="watchlist-row">
           <div class="col-lg-6">
             <div class="account-card watchlist">
               <h3>Watchlist Name</h3>
               <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Symbol</th>
+                    <th>Change %</th>
+                    <th>Volume</th>
+                  </tr>
+                </thead>
                 <tbody>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">AKS</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.76%</span></td>
+                    <td>12.79M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CIEN</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.33%</span></td>
+                    <td>2.63M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">CLF</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.40%</span></td>
+                    <td>17.39M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">FCX</a></td>
+                    <td><span class="ml-0 d-initial change up">+1.63%</span></td>
+                    <td>68.00M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">INTC</a></td>
+                    <td><span class="ml-0 d-initial change down">-0.56%</span></td>
+                    <td>60.73M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">RIG</a></td>
+                    <td><span class="ml-0 d-initial change up">+11.30%</span></td>
+                    <td>38.90M</td>
                   </tr>
                   <tr>
-                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/us.svg"></td>
-                    <td>AAPL</td>
-                    <td>Apple Computers Inc.</td>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">S</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.18%</span></td>
+                    <td>43.60M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">T</a></td>
+                    <td><span class="ml-0 d-initial change down">-1.43%</span></td>
+                    <td>121.10M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">TEVA</a></td>
+                    <td><span class="ml-0 d-initial change down">-5.57%</span></td>
+                    <td>30.15M</td>
+                  </tr>
+                  <tr>
+                    <td><img class="country-flag-md" src="/static/main_app/svg/flags/ch.svg" alt=""></td>
+                    <td><a href="">X</a></td>
+                    <td><span class="ml-0 d-initial change up">+3.22%</span></td>
+                    <td>21.49M</td>
                   </tr>
                 </tbody>
               </table>
@@ -246,7 +343,6 @@ var html_templates = {
             </a>
           </div>
         </div>
-
       </div>
     </div>
     `
